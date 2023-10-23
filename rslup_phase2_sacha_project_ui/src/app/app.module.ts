b8b4b0368728b0da57b903ad5d2fcdd901ed491b
Your Name <a.cooper8789@gmail.com>
2023-10-23 22:06:31 +0100

    added basic components

--- rslup_phase2_sacha_project_ui/src/app/app.module.ts
+++ rslup_phase2_sacha_project_ui/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegistrationComponentComponent } from './registration-component/registration-component.component';
+import { LoginComponentComponent } from './login-component/login-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrationComponentComponent,
+    LoginComponentComponent
   ],
   imports: [
     BrowserModule,
